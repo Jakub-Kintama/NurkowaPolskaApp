958ef1b52dbf9ab69be5d636575a2252732711dd
Jakub Krzynowek <kubaisnow@gmail.com>
2023-10-13 20:36:55 +0200

    API init

---
+++ API/src/main/kotlin/pjatk/pjwstk/pl/api/ApiApplication.kt
@@ -0,0 +1,11 @@
+package pjatk.pjwstk.pl.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiApplication>(*args)
+}
---
+++ API/src/test/kotlin/pjatk/pjwstk/pl/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package pjatk.pjwstk.pl.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

d0f173786c96b064d589985f4f5f8318e3948643
Jakub Krzynowek <kubaisnow@gmail.com>
2023-10-14 21:52:02 +0200

    Web Layer
    +MockMvc
    +Postman

--- API/src/main/kotlin/pjatk/pjwstk/pl/api/controllers/guest/GuestMarkersController.kt
+++ API/src/main/kotlin/pjatk/pjwstk/pl/api/controllers/guest/GuestMarkersController.kt
@@ -4,14 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import pjatk.pjwstk.pl.api.model.Marker
+import pjatk.pjwstk.pl.api.service.MarkerService
 
 @RestController
 @RequestMapping("api/markers")
-class GuestMarkersController {
+class GuestMarkersController(private val service: MarkerService) {
     @GetMapping
-    fun getAllMarkers(): String {
-        return "get all markers"
-    }
+    fun getMarkers(): Collection<Marker> = service.getMarkers()
 
     @GetMapping("/{id}")
     fun getMarkerById(@PathVariable id: String): String {

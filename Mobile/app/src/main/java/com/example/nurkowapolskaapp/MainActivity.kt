acaa346ffd18818d027b5e34e37ca82a3a4fb2b9
Knowacki <knowacki.main@gmail.com>
2023-10-22 22:32:05 +0200

    Zmiany struktury Mobile pod używanie Jetpack Compose

--- Mobile/app/src/main/java/com/example/nurkowapolskaapp/MainActivity.kt
+++ Mobile/app/src/main/java/com/example/nurkowapolskaapp/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.nurkowapolskaapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.nurkowapolskaapp.ui.theme.NurkowaPolskaAppTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            NurkowaPolskaAppTheme() {
+                HelloWorld()
+            }
+        }
     }
+}
+
+@Composable
+fun HelloWorld() {
+    Text(text = "Hello World")
+}
+
+@Preview
+@Composable
+fun HelloWorldPreview() {
+    HelloWorld()
 }

c0afda9ef5b91c3ee461b5ee5763d918d4bb7af8
JSkrzynski <jakubskrzynski18@gmail.com>
2023-10-29 19:45:33 +0100

    switched to React

---
+++ admin-panel/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="topnav">
+        <button className='topnavButton' id='homeButton'>Powrót</button>
+        <button className='topnavButton' id='exportButton'>Eksportuj znaczniki</button>
+      </div>
+      <div className=''>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;

e7b49388838f1969b8ceb3d8af9210e2f68821e9
Knowacki <knowacki.main@gmail.com>
2023-11-14 11:42:41 +0100

    Pierwsza faktyczna wersja aplikacji mobilnej z mapą

--- Mobile/app/build.gradle.kts
+++ Mobile/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.utils.loadPropertyFromResources
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -40,7 +42,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
+        kotlinCompilerExtensionVersion = "1.5.4"
     }
     packaging {
         resources {
@@ -50,21 +52,30 @@ android {
 }
 
 dependencies {
-    implementation("androidx.navigation:navigation-compose:2.7.4")
-    implementation("androidx.compose.material:material-icons-extended:1.5.4")
+    // CredentialManager
+    implementation("androidx.credentials:credentials:1.2.0")
+    // GoogleSign
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.0")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    // GoogleMap
+    implementation("com.google.maps.android:maps-compose:4.1.1")
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.5")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2023.10.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
--- Mobile/app/src/main/java/com/example/nurkowapolskaapp/MainActivity.kt
+++ Mobile/app/src/main/java/com/example/nurkowapolskaapp/MainActivity.kt
@@ -3,29 +3,87 @@ package com.example.nurkowapolskaapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.nurkowapolskaapp.app.functions.FirstAid
+import com.example.nurkowapolskaapp.app.functions.Insurance
+import com.example.nurkowapolskaapp.app.functions.maps.AddMarker
+import com.example.nurkowapolskaapp.app.functions.maps.MarkersMap
 import com.example.nurkowapolskaapp.ui.theme.NurkowaPolskaAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            NurkowaPolskaAppTheme() {
-                HelloWorld()
+            NurkowaPolskaAppTheme {
+                AppScaffold()
+            }
+        }
+    }
+
+}
+
+// OptIn potrzebny bo z jakiegoś powodu BottomAppBar jest traktowany jako eksperymentalny feature
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppScaffold() {
+    val navController = rememberNavController()
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = { Text(text = "Default") },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Menu, null)
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                    }
+                }
+            )
+        },
+    ) { innerPadding ->
+        Column(Modifier.padding(innerPadding))
+        {
+            NavHost(navController, startDestination = "homepage") {
+                composable("homepage") { Homepage(navController) }
+                composable("mapOfMarkers") { MarkersMap(navController) }
+                composable("insurance") { Insurance(navController) }
+                composable("firstAid") { FirstAid(navController) }
+                composable("addMarker") { AddMarker(navController) }
             }
         }
     }
 }
 
+
+
+@Preview(showSystemUi = false, showBackground = false)
 @Composable
-fun HelloWorld() {
-    Text(text = "Hello World")
+fun AppScaffoldPreview() {
+    AppScaffold()
 }
 
-@Preview
-@Composable
-fun HelloWorldPreview() {
-    HelloWorld()
-}

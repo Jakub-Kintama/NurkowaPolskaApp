cad7340a461a68fea8bbf05c627794b4f0370ba7
JSkrzynski <jakubskrzynski18@gmail.com>
2023-11-21 01:27:06 +0100

    added functionality to detail popup

--- admin-panel/src/components/MarkerTable.js
+++ admin-panel/src/components/MarkerTable.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
+import DetailsPopup from "./DetailsPopup";
 
 export default function MarkerTable({markers}) {
+
+    const [DetailsPopupButton, setDetailsPopupButton] = useState(false);
+    const [markerDetails, setMarkerDetails] = useState([])
+
     return(
     <div className="MarkerList">
         <h2>Lista znaczników:</h2>
@@ -19,13 +24,14 @@ export default function MarkerTable({markers}) {
                         <td>{marker.mapMarker.position.lat}, {marker.mapMarker.position.lng}</td>
                         <td>{marker.CrayfishType}</td>
                         <td>{marker.verified ? "Zweryfikowany" : "Niezweryfikowany" }</td>
-                        <td><button className="TableButton">Szczegóły</button></td>
+                        <td><button className="TableButton" onClick={ () => {setDetailsPopupButton(true); setMarkerDetails([marker._id, marker.mapMarker.position.lat,marker.mapMarker.position.lng, marker.date, marker.CrayfishType, marker.mapMarker.title, marker.mapMarker.description, marker.userEmail])}}>Szczegóły</button></td>
                     </tr>
                     </>
                 ))}
             </tbody>
             
-        </table>            
+        </table>
+        <DetailsPopup trigger={DetailsPopupButton} setTrigger={setDetailsPopupButton} marker={markerDetails}/>          
     </div>
     )
 }

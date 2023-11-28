56f4d3735ed1a55036d95b9c81d5e9f18c97f2e9
JSkrzynski <jakubskrzynski18@gmail.com>
2023-11-28 03:16:47 +0100

    New Features and Fixes
    Fixed DOM errors, added a new popup to edit markers, added a component for a logged user who is not an admin

--- admin-panel/src/components/AdminTable.js
+++ admin-panel/src/components/AdminTable.js
@@ -1,35 +1,39 @@
 import React, {useState} from "react";
-import AddPopup from "./AddPopup";
-import EditPopup from "./EditPopup";
+import AddAdminPopup from "./AddAdminPopup";
+import EditAdminPopup from "./EditAdminPopup";
 
 export default function AdminTable({markers}) {
 
-    const [AddPopupButton, setAddPopupButton] = useState(false);
-    const [EditPopupButton, setEditPopupButton] = useState(false);
+    const [AddAdminPopupButton, setAddAdminPopupButton] = useState(false);
+    const [EditAdminPopupButton, setEditAdminPopupButton] = useState(false);
     const [adminDetails, setAdminDetails] = useState([])
 
     return(
         <div>
             <h2>Lista osób uprawnionych:</h2>
             <table className="AdminTable">
-                <tr>
-                    <th>E-Mail</th>
-                    <th>Imię</th>
-                    <th><button onClick={() => setAddPopupButton(true)} className="TableButton">Dodaj</button></th>
-                </tr>
-                <tr>
-                    <td>*E-Mail osoby uprawnionej*</td>
-                    <td>*Imię*</td>
-                    <td><button onClick={() => {setEditPopupButton(true); setAdminDetails(["*E-Mail osoby uprawnionej*","*Imię*"])}} className="TableButton">Edytuj</button></td>
-                </tr>
-                <tr>
-                    <td>*Inny E-Mail*</td>
-                    <td>*Inne Imię*</td>
-                    <td><button onClick={() => {setEditPopupButton(true); setAdminDetails(["*Inny E-Mail*","*Inne Imię*"])}} className="TableButton">Edytuj</button></td>
-                </tr>
+                <thead>
+                    <tr>
+                        <th>E-Mail</th>
+                        <th>Imię</th>
+                        <th><button onClick={() => setAddAdminPopupButton(true)} className="TableButton">Dodaj</button></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>*E-Mail osoby uprawnionej*</td>
+                        <td>*Imię*</td>
+                        <td><button onClick={() => {setEditAdminPopupButton(true); setAdminDetails(["*E-Mail osoby uprawnionej*","*Imię*"])}} className="TableButton">Edytuj</button></td>
+                    </tr>
+                    <tr>
+                        <td>*Inny E-Mail*</td>
+                        <td>*Inne Imię*</td>
+                        <td><button onClick={() => {setEditAdminPopupButton(true); setAdminDetails(["*Inny E-Mail*","*Inne Imię*"])}} className="TableButton">Edytuj</button></td>
+                    </tr>
+                </tbody>
             </table>
-            <AddPopup trigger={AddPopupButton} setTrigger={setAddPopupButton}></AddPopup>
-            <EditPopup trigger={EditPopupButton} setTrigger={setEditPopupButton} email={adminDetails[0]} name={adminDetails[1]}></EditPopup>
+            <AddAdminPopup trigger={AddAdminPopupButton} setTrigger={setAddAdminPopupButton}></AddAdminPopup>
+            <EditAdminPopup trigger={EditAdminPopupButton} setTrigger={setEditAdminPopupButton} email={adminDetails[0]} name={adminDetails[1]}></EditAdminPopup>
         </div>
     )
 }
--- admin-panel/src/components/EditMarkerPopup.js
+++ admin-panel/src/components/EditMarkerPopup.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-export default function EditPopup(props) {
+export default function EditMarkerPopup(props) {
     return (props.trigger) ? (
         <div className="Popup">
             <div className="PopupInner">
                 <button onClick={ () => props.setTrigger(false) } className="CloseButton">Zamknij</button>
                 <h2>Edytuj</h2>
-                <input type="text" value={props.email}></input>
-                <input type="text" value={props.name}></input>
+                <input type="text" value={props.lat}></input>
+                <input type="text" value={props.lng}></input>
+                <input type="text" value={props.date}></input>
+                <input type="text" value={props.type}></input>
                 <button>Submit</button>
             </div>
         </div>

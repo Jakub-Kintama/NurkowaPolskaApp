e9a0760eaa56bb0dd5876268d20758d1545724d0
JSkrzynski <jakubskrzynski18@gmail.com>
2023-12-11 19:25:38 +0100

    Made popup code cleaner

--- admin-panel/src/components/popups/DetailsPopup.js
+++ admin-panel/src/components/popups/DetailsPopup.js
@@ -7,12 +7,12 @@ export default function DetailsPopup(props) {
             <div className="PopupInner">
                 <button onClick={ () => props.setTrigger(false) } className="CloseButton">Zamknij</button>
                 <h2>Znacznik</h2>
-                <iframe title={props} src={`https://maps.google.com/maps?q=${props.marker[1]},${props.marker[2]}&z=14&output=embed`} style={{width: 600, height: 450,border: 0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-                <p><strong>Data dodania: </strong>{props.marker[3]}</p>
-                <p><strong>Typ: </strong>{crayfishTypeSwitch(props.marker[4])}</p>
-                <p><strong>Tytuł: </strong>{props.marker[5]}</p>
-                <p><strong>Opis: </strong>{props.marker[6]}</p>
-                <p><strong>Dodane przez: </strong>{props.marker[7]}</p>
+                <iframe title={props} src={`https://maps.google.com/maps?q=${props.marker.lat},${props.marker.lng}&z=14&output=embed`} style={{width: 600, height: 450,border: 0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                <p><strong>Data dodania: </strong>{props.marker.date}</p>
+                <p><strong>Typ: </strong>{crayfishTypeSwitch(props.marker.crayfishType)}</p>
+                <p><strong>Tytuł: </strong>{props.marker.title}</p>
+                <p><strong>Opis: </strong>{props.marker.description}</p>
+                <p><strong>Dodane przez: </strong>{props.marker.userEmail}</p>
             </div>
         </div>
     ) : "";

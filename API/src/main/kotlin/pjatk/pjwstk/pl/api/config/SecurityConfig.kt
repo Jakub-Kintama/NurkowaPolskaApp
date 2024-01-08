10884ee1f976bd7cb12f076306b2899ad89b19a6
Jakub Krzynowek <kubaisnow@gmail.com>
2024-01-08 21:45:10 +0100

    google oauth

--- API/src/main/kotlin/pjatk/pjwstk/pl/api/config/SecurityConfig.kt
+++ API/src/main/kotlin/pjatk/pjwstk/pl/api/config/SecurityConfig.kt
@@ -2,62 +2,49 @@ package pjatk.pjwstk.pl.api.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
-import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.web.DefaultSecurityFilterChain
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.filter.CorsFilter
-import pjatk.pjwstk.pl.api.config.jwt.JwtAuthenticationFilter
-import pjatk.pjwstk.pl.api.service.oauth2.GoogleOAuth2UserService
 
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig(
-    private val authenticationProvider: AuthenticationProvider,
-    private val googleOAuth2UserService: GoogleOAuth2UserService
-) {
+class WebConfig{
 
     @Bean
-    fun securityFilterChain(
-        http: HttpSecurity, jwtAuthenticationFilter: JwtAuthenticationFilter
-    ): DefaultSecurityFilterChain = http.csrf {
-        it.disable()
-    }
-        .cors {
-            corsFilter()
-        }
-        .authorizeHttpRequests {
-            it.requestMatchers(HttpMethod.GET, "/api/markers", "/api/markers/**").permitAll()
-                .requestMatchers("/api/markers", "api/markers/**").fullyAuthenticated()
-                .requestMatchers("/api/admin/markers", "/api/admin/markers/**", "/api/users", "/api/users/**")
-                .hasRole("ADMIN").anyRequest().permitAll()
-        }
-        .sessionManagement {
-            it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        }
-        .authenticationProvider(authenticationProvider)
-        .oauth2Login { it ->
-            it.userInfoEndpoint{
-                it.userService(googleOAuth2UserService)
-            }
-        }
-        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java).build()
-
-    @Bean
-    fun corsFilter(): CorsFilter {
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOriginPatterns = listOf("*")
+        configuration.allowedMethods = listOf("*")
+        configuration.allowedHeaders = listOf("*")
+        configuration.allowCredentials = true
         val source = UrlBasedCorsConfigurationSource()
-        val config = CorsConfiguration()
-        config.allowCredentials = true
-        config.allowedOrigins = listOf("http://localhost:3000/*", "http://localhost:8080/*")
-        config.addAllowedHeader("*")
-        config.addAllowedMethod("*")
-        source.registerCorsConfiguration("/**", config)
-        return CorsFilter(source)
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .authorizeHttpRequests { registry ->
+                registry
+                    .requestMatchers("/login", "/ping")
+                    .permitAll()
+                registry
+                    .anyRequest()
+                    .authenticated()
+            }
+            .csrf { it.disable() }
+            .cors { c -> c.configurationSource(corsConfigurationSource()) }
+            .sessionManagement{
+                it.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+            }
+            .oauth2Login { l -> l.defaultSuccessUrl("http://localhost:8080/me", true) }
+            .formLogin{}
+            .build()
     }
 }
--- API/src/main/kotlin/pjatk/pjwstk/pl/api/config/jwt/JwtProperties.kt
+++ API/src/main/kotlin/pjatk/pjwstk/pl/api/config/jwt/JwtProperties.kt
@@ -1,10 +1,10 @@
-package pjatk.pjwstk.pl.api.config.jwt
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties("jwt")
-data class JwtProperties(
-    val key: String? = System.getenv("JWT_SECRET"),
-    val accessTokenExpiration: Long,
-    val refreshTokenExpiration: Long
-)
+//package pjatk.pjwstk.pl.api.config.jwt
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties
+//
+//@ConfigurationProperties("jwt")
+//data class JwtProperties(
+//    val key: String? = System.getenv("JWT_SECRET"),
+//    val accessTokenExpiration: Long,
+//    val refreshTokenExpiration: Long
+//)
